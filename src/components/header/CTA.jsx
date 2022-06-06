0ab93be0de288e08da189ff19a581c8c98cc9cbd
MarynaPR <pryadkamaryna@gmail.com>
2022-06-05 22:43:42 -0500

    cv removed

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CV from '../../images/hand-CV.pdf'
+// import CV from '../../images/hand-CV.pdf'
 
 const CTA = () => {
     return (
         <div className='cta'>
-            <a href={CV} download className='btn'>Download CV</a>
-            <a href="#contact" className='btn btn-primary'> Let's Talk</a>
+            {/* <a href={CV} download className='btn'>Download CV</a> */}
+            <a href="#contact" className='btn'> Let's Talk</a>
         </div>
     )
 }
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BsLinkedin } from 'react-icons/bs'
-import { BsGithub } from 'react-icons/bs'
+// import { BsLinkedin } from 'react-icons/bs'
+// import { BsGithub } from 'react-icons/bs'
 import { AiFillInstagram } from 'react-icons/ai'
 
 function HeaderSocials() {
